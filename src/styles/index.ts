9ad03444f952f1393d9d981a24adbee07986046c
Ryan <ryangabriel_cap@hotmail.com>
2024-09-08 22:44:15 +0100

    style: Adicionado variáveis das cores que vamos utilizar.

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -12,7 +12,14 @@ export const {
 } = createStitches({
   theme: {
     colors: {
-      red: "red",
+      white: "#fff",
+      gray900: "#121214",
+      gray800: "#202024",
+      gray300: "#c4c4cc",
+      gray100: "#e1e1e6",
+
+      purple500: "#7D3FBF",
+      purple300: "#9A61D7" 
     },
   },
 });
