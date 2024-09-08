83f325f326199587699e6c6f7f5c713347cbaba8
Ryan <ryangabriel_cap@hotmail.com>
2024-09-08 22:25:15 +0100

    test: "Simulando botão para testar funcionamento do Stitches."

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,27 @@
+import { styled } from "@/styles";
+
+const Button = styled("button", {
+  backgroundColor: "$red",
+  borderRadius: 4,
+  border: 0,
+  padding: "4px 8px",
+  cursor: "pointer",
+
+  span: {
+    fontWeight: "bold",
+    marginRight: "20px",
+  },
+
+  "&:hover": {
+    filter: "brightness(0.8)",
+  },
+});
+
 export default function Home() {
   return (
-   <h1>Hello World</h1>
+    <Button>
+      <span>Teste</span>
+      Enviar
+    </Button>
   );
 }
