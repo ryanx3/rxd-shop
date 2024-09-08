552dc291526a5464e95289e98cb97de04ec3c757
Ryan <ryangabriel_cap@hotmail.com>
2024-09-08 22:43:53 +0100

    style: Adicionado função globalStyles() para funcionar em toda a aplicação no arquivo "_app.tsx".

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,8 @@
-export default function App({ Component, pageProps }) {
+import { globalStyles } from "@/styles/global";
+import { AppProps } from "next/app";
+
+globalStyles()
+
+export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
