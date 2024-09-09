9b69e299cb4dac218973a7295b520586e631b561
Ryan <ryangabriel_cap@hotmail.com>
2024-09-09 22:46:09 +0100

    feat: Alterando página Home para receber nossos componentes e produtos utilizando o keen-slider como carrossel e o Image do next.js para melhor performance.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,53 @@
-import { styled } from "@/styles";
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
 
-const Button = styled("button", {
-  backgroundColor: "$red",
-  borderRadius: 4,
-  border: 0,
-  padding: "4px 8px",
-  cursor: "pointer",
+import { useKeenSlider } from "keen-slider/react";
+import "keen-slider/keen-slider.min.css";
 
-  span: {
-    fontWeight: "bold",
-    marginRight: "20px",
-  },
-
-  "&:hover": {
-    filter: "brightness(0.8)",
-  },
-});
+import tshirt1 from "../assets/tshirts/1.png";
+import tshirt2 from "../assets/tshirts/2.png";
+import tshirt3 from "../assets/tshirts/3.png";
 
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  });
+
   return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={tshirt1} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tshirt2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className="keen-slider__slide">
+        <Image src={tshirt3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={tshirt3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
   );
 }
