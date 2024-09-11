2c644f3572d680745242e003d0542947fa5dfaef
Ryan <ryangabriel_cap@hotmail.com>
2024-09-11 21:36:45 +0100

    feat: Adicionado componentes criados no Stitches e incluido informações estáticas no componente para testes.

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,29 @@
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "@/styles/pages/product";
 import { useRouter } from "next/router";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product: {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>79,99</span>
+
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate,
+          sit! Provident, ipsam eligendi error officiis velit quisquam culpa
+          repellendus! Dolorem, laboriosam impedit magni obcaecati quas hic
+          earum qui sunt labore.
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
