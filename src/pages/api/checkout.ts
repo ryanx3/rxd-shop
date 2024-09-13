74681a5efab04ad87669a2b35979252e03398d48
Ryan <ryangabriel_cap@hotmail.com>
2024-09-13 21:32:14 +0100

    feat: Importado ID dinâmico para o checkout e feito verificações de confirmação.

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,19 +5,32 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const priceId = "price_1PxbDWFghDF6WigyY3l88tz6";
+  const { priceId } = req.body;
 
-  const successUrl = `${process.env.NEXT_URL}/sucess`
-  const cancelUrl = `${process.env.NEXT_URL}/`
+  if (req.method !== "POST") {
+    return res.status(405).json({ error: "Method not allowed." });
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: "Price not found." });
+  }
+
+  const successUrl = `${process.env.NEXT_URL}/success`;
+  const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
     cancel_url: cancelUrl,
     mode: "payment",
-    line_items: [{ price: priceId, quantity: 1 }],
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      },
+    ],
   });
 
   return res.status(201).json({
-    checkoutUrl: checkoutSession.url
-  })
+    checkoutUrl: checkoutSession.url,
+  });
 }
