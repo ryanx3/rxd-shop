f14f01de63943622896bed8b44430a1ffcdf5321
Ryan <ryangabriel_cap@hotmail.com>
2024-09-13 21:41:37 +0100

    feat: Estrutura e estilização da página Success.

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,19 @@
+import Link from "next/link";
+import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+
 export default function Success() {
-  return <h1>Success</h1>;
+  return (
+    <SuccessContainer>
+        <h1>Compra efetuada</h1>
+
+        <ImageContainer></ImageContainer>
+
+        <p>
+          Uhuul <strong>Ryan Martins</strong>, sua{" "}
+          <strong>T-shirt | Aboard</strong> já está a caminho da sua casa.
+        </p>
+
+        <Link href="/">Voltar ao catálogo</Link>
+      </SuccessContainer>
+  );
 }
