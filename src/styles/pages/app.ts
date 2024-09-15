bb992bd25e0ce359cd4de1284f26bc6f50436941
Ryan <ryangabriel_cap@hotmail.com>
2024-09-15 21:11:05 +0100

    feat: Criado componente CartButton

--- src/styles/pages/app.ts
+++ src/styles/pages/app.ts
@@ -13,4 +13,43 @@ export const Header = styled("header", {
   width: "100%",
   maxWidth: 1180,
   margin: "0 auto",
+  display: "flex",
+  justifyContent: "space-between",
+
+  svg: {
+    color: "$gray300",
+  },
+});
+
+export const CartButton = styled("button", {
+  padding: "1rem",
+  borderRadius: "6px",
+  cursor: "pointer",
+  border: "none",
+
+  variants: {
+    variant: {
+      purple: {
+        backgroundColor: "$purple500",
+      },
+      gray: {
+        backgroundColor: "$gray800",
+      },
+    },
+  },
+  
+  defaultVariants: {
+    variant: "gray",
+  },
+  
+  
+  svg: {
+    color: "$gray100",
+  },
+  
+  transition: "all 0.1s ease-in-out",
+  
+  "&:hover": {
+    opacity: 0.8,
+  },
 });
