cd12f93a5b361b467ea5bd50e362855a3ccfe20b
Ryan <ryangabriel_cap@hotmail.com>
2024-09-17 22:43:22 +0100

    feat: Adicionando produtos dinamicos ao SideMenu

--- src/components/SideMenu.tsx
+++ src/components/SideMenu.tsx
@@ -1,9 +1,11 @@
+import { useCart } from "@/context/CartContext";
 import {
   CartContent,
   CartFooter,
   CartItem,
   SideMenuContainer,
 } from "@/styles/components/SideMenu";
+import axios from "axios";
 import { X } from "lucide-react";
 import Image from "next/image";
 
@@ -13,6 +15,26 @@ interface SideMenuProps {
 }
 
 export default function SideMenu({ open, onClose }: SideMenuProps) {
+  const { products, removeFromCart } = useCart();
+  
+  async function handleBuyButton() {
+    try {
+      const response = await axios.post("/api/checkout", {
+        priceId: product.defaultPriceId,
+      });
+      const { checkoutUrl } = response.data;
+      window.location.href = checkoutUrl;
+    } catch (error) {
+      alert("Falha ao redirecionar ao checkout.");
+    }
+  }
+
+const totalPrice = products.reduce(
+  (total, { price }) =>
+    total + parseFloat(price.replace("€", "").replace(",", ".")),
+  0
+);
+
   return (
     <SideMenuContainer open={open}>
       <header>
@@ -23,103 +45,48 @@ export default function SideMenu({ open, onClose }: SideMenuProps) {
       </header>
 
       <CartContent>
-        <CartItem>
-          <Image
-            src="/static-img.png"
-            alt="T-Shirt One"
-            width={94}
-            height={94}
-          />
-          
-          <div>
-            <span>Tshirt | Space lab</span>
-            <strong>R$ 79,90</strong>
-            <button>Remover</button>
-          </div>
-        </CartItem>
-        <CartItem>
-          <Image
-            src="/static-img.png"
-            alt="T-Shirt One"
-            width={94}
-            height={94}
-          />
+        {products.length === 0 ? (
+          <p>Seu carrinho está vazio</p>
+        ) : (
+          products.map((product) => (
+            <CartItem key={product.id}>
+              <Image
+                src={product.imageUrl}
+                alt={product.name}
+                width={94}
+                height={94}
+              />
 
-          <div>
-            <span>Tshirt | Space lab</span>
-            <strong>R$ 79,90</strong>
-            <button>Remover</button>
-          </div>
-        </CartItem>
-        <CartItem>
-          <Image
-            src="/static-img.png"
-            alt="T-Shirt One"
-            width={94}
-            height={94}
-          />
-
-          <div>
-            <span>Tshirt | Space lab</span>
-            <strong>R$ 79,90</strong>
-            <button>Remover</button>
-          </div>
-        </CartItem>
-        <CartItem>
-          <Image
-            src="/static-img.png"
-            alt="T-Shirt One"
-            width={94}
-            height={94}
-          />
-
-          <div>
-            <span>Tshirt | Space lab</span>
-            <strong>R$ 79,90</strong>
-            <button>Remover</button>
-          </div>
-        </CartItem>
-        <CartItem>
-          <Image
-            src="/static-img.png"
-            alt="T-Shirt One"
-            width={94}
-            height={94}
-          />
-
-          <div>
-            <span>Tshirt | Space lab</span>
-            <strong>R$ 79,90</strong>
-            <button>Remover</button>
-          </div>
-        </CartItem>
-        <CartItem>
-          <Image
-            src="/static-img.png"
-            alt="T-Shirt One"
-            width={94}
-            height={94}
-          />
-
-          <div>
-            <span>Tshirt | Space lab</span>
-            <strong>R$ 79,90</strong>
-            <button>Remover</button>
-          </div>
-        </CartItem>
+              <div>
+                <span>{product.name}</span>
+                <strong>{product.price}</strong>
+                <button onClick={() => removeFromCart(product.id)}>
+                  Remover
+                </button>
+              </div>
+            </CartItem>
+          ))
+        )}
       </CartContent>
 
-      <CartFooter>
-        <div>
-          <span>Quantidade</span>
-          <span>3 itens</span>
-        </div>
-        <div>
-          <strong>Valor total</strong>
-          <strong>R$ 270,00</strong>
-        </div>
-        <button>Finalizar compra</button>
-      </CartFooter>
+      {products.length > 0 && (
+        <CartFooter>
+          <div>
+            <span>Quantidade</span>
+            <span>{products.length} item(s)</span>{" "}
+          </div>
+          <div>
+            <strong>Valor total</strong>
+            <strong>
+              {new Intl.NumberFormat("pt-PT", {
+                style: "currency",
+                currency: "EUR",
+              }).format(totalPrice)}
+            </strong>{" "}
+          </div>
+          <button onClick={handleBuyButton}>Finalizar compra</button>
+        </CartFooter>
+      )}
     </SideMenuContainer>
   );
 }
