ceca3ce8b4e85717adc65f78d5cba43a4e7dca09
Ryan <ryangabriel_cap@hotmail.com>
2024-09-17 22:42:57 +0100

    feat: Aplicado contexto para toda a aplicação.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import Link from "next/link";
 import SideMenu from "@/components/SideMenu";
 import { useState } from "react";
+import { CartProvider } from "@/context/CartContext";
 
 globalStyles();
 
@@ -14,20 +15,22 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   const [isCartOpen, setIsCartOpen] = useState(false);
 
   return (
-    <Container>
-      <Header>
-        <Link href={"/"}>
-          <Image src={LogoImg} alt="Logo RxD" />
-        </Link>
-        <CartButton onClick={() => setIsCartOpen(!isCartOpen)} variant="gray">
-          <ShoppingBag size={24} />
-          <span className="badge">1</span>
-        </CartButton>
-      </Header>
+    <CartProvider>
+      <Container>
+        <Header>
+          <Link href={"/"}>
+            <Image src={LogoImg} alt="Logo RxD" />
+          </Link>
+          <CartButton onClick={() => setIsCartOpen(!isCartOpen)} variant="gray">
+            <ShoppingBag size={24} />
+            <span className="badge">1</span>
+          </CartButton>
+        </Header>
 
-      <SideMenu open={isCartOpen} onClose={() => setIsCartOpen(false)} />
+        <SideMenu open={isCartOpen} onClose={() => setIsCartOpen(false)} />
 
-      <Component {...pageProps} />
-    </Container>
+        <Component {...pageProps} />
+      </Container>
+    </CartProvider>
   );
 }
