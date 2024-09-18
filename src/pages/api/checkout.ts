02a278eba55fb27a566735ad0532bb5528e0fe48
Ryan <ryangabriel_cap@hotmail.com>
2024-09-18 23:45:18 +0100

    feat: Alterando handler do checkout para receber múltiplos produtos.

--- src/pages/api/checkout.ts
+++ src/pages/api/checkout.ts
@@ -5,30 +5,27 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { priceId } = req.body;
+  const { lineItems } = req.body;
 
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Method not allowed." });
   }
 
-  if (!priceId) {
+  if (!lineItems || lineItems.length === 0) {
     return res.status(400).json({ error: "Price not found." });
   }
 
   const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`;
-
   const cancelUrl = `${process.env.NEXT_URL}/`;
 
   const checkoutSession = await stripe.checkout.sessions.create({
     success_url: successUrl,
     cancel_url: cancelUrl,
     mode: "payment",
-    line_items: [
-      {
-        price: priceId,
-        quantity: 1,
-      },
-    ],
+    line_items: lineItems.map((item: { priceId: string }) => ({
+      price: item.priceId,
+      quantity: 1,
+    })),
   });
 
   return res.status(201).json({
