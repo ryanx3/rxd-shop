e40a18c63ac669e35c9425a770acdf20b6b46a22
Ryan <ryangabriel_cap@hotmail.com>
2024-09-22 20:48:15 +0100

    refactory: Adicionando componente Header separado do MyApp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,35 +1,21 @@
 import { globalStyles } from "@/styles/global";
 import { AppProps } from "next/app";
-import { CartButton, Header, Container } from "@/styles/pages/app";
-import { ShoppingBag } from "lucide-react";
-import LogoImg from "../assets/logo.svg";
-import Image from "next/image";
-import Link from "next/link";
+import { Container } from "@/styles/pages/app";
 import SideMenu from "@/components/SideMenu";
 import { useState } from "react";
 import { CartProvider } from "@/context/CartContext";
+import Header from "@/components/Header";
 
 globalStyles();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   const [isCartOpen, setIsCartOpen] = useState(false);
 
-  
   return (
     <CartProvider>
       <Container>
-        <Header>
-          <Link href={"/"}>
-            <Image src={LogoImg} alt="Logo RxD" />
-          </Link>
-          <CartButton onClick={() => setIsCartOpen(!isCartOpen)} variant="gray">
-            <ShoppingBag size={24} />
-            <span className="badge">1</span>
-          </CartButton>
-        </Header>
-
+        <Header onToggleCart={() => setIsCartOpen((prev) => !prev)} />
         <SideMenu open={isCartOpen} onClose={() => setIsCartOpen(false)} />
-
         <Component {...pageProps} />
       </Container>
     </CartProvider>
